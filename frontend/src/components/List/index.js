1d72b726fb708ed19cfc61b808fbee7329afea32
Celvine Adi Putra <Celvineadiputra2@gmail.com>
2021-09-22 19:39:30 +0700

    Add Tailwind css

--- frontend/src/components/List/index.js
+++ frontend/src/components/List/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function List(props) {
     return (
-        <div>
-            LIST
+        <div className="flex">
+            <diV>
+                <h1 className="text-5xl">LIST BOOK</h1>
+            </diV>
+            <diV>
+                <Link to="/book/new" className="bg-blue-500 text-white p-5 text-justify align-center rounded">Add new Book</Link>
+            </diV>
         </div>
     )
 }
