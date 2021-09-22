d826cd8be803d7f89516685e26460141d921b966
Celvine Adi Putra <Celvineadiputra2@gmail.com>
2021-09-22 19:27:49 +0700

    init Frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Form from "./components/Form"
+import List from "./components/List"
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/book/:id/edit" component={Form} />
+          <Route path="/book/new" component={Form} />
+          <Route path="/books" component={List} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Form/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Form(props) {
+    return (
+        <div>
+            FORM
+        </div>
+    )
+}
+
+export default Form
